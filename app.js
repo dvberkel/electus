8292f3e831f7af6f833c878e386f64e8536f2832
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-12-05 22:21:01 +0100

    heroku only supports long polling :(

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var io = require("socket.io").listen(process.env.PORT || 3435);
 
+io.configure(function () { 
+  io.set("transports", ["xhr-polling"]); 
+  io.set("polling duration", 10); 
+});
+
 io.sockets.on('connection', function (socket) {
     socket.on('sentence', function(data){
 	console.log(data);
