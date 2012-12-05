770120a83e7493708b49ffd7e71bde8797c5e265
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-12-05 21:15:07 +0100

    prepare app.js for the communication protocal'

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var io = require("socket.io").listen(3435);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('sentence', { text: 'It is better to ask forgiveness then permission' });
+    socket.on('sentence', function(data){
+	console.log(data);
+	socket.broadcast.emit("sentence", data);
+    });
     socket.on("agreement", function(data){
 	console.log(data);
+	socket.broadcast.emit("agreement", data);
     })
 });
