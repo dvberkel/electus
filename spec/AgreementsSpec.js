6448986c908e3d7c586a0ef86eda405e25b7da80
dvberkel <daan.vanberkel@nedap.com>
2012-12-06 12:03:47 +0100

    adhere to the grunt standard

--- spec/AgreementsSpec.js
+++ spec/AgreementsSpec.js
@@ -72,61 +72,61 @@ describe("Agreements", function(){
         agreements.addAgreements("You should come prepared", true);
         agreements.addAgreements("It is better to ask forgiveness then permission", false);
         
-	expect(agreements.length).toBe(2);
+        expect(agreements.length).toBe(2);
         var agreement = agreements.agreementWithText("You should come prepared");
         expect(agreement).toBeDefined();
         expect(agreement.agreements()).toBe(2);
         expect(agreement.disagreements()).toBe(1);
-	expect(agreement.votes()).toBe(3);
+        expect(agreement.votes()).toBe(3);
     });
 
 
     describe("AgreementsView", function(){
-	var agreements;
-	
-	beforeEach(function(){
-	    loadFixtures("viewport.html");
-	});
+        var agreements;
+        
+        beforeEach(function(){
+            loadFixtures("viewport.html");
+        });
 
-	beforeEach(function(){
-	    agreements = new Electus.Agreements();
-	    agreements.addAgreements("You should come prepared", false);
-	});
-	
-	it("should create a container", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements")).toExist();
-	});
+        beforeEach(function(){
+            agreements = new Electus.Agreements();
+            agreements.addAgreements("You should come prepared", false);
+        });
+        
+        it("should create a container", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements")).toExist();
+        });
 
-	it("should have a table row for each model", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements tr.agreement")).toExist();
-	});
+        it("should have a table row for each model", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements tr.agreement")).toExist();
+        });
 
-	it("each row should have an upvote cell", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements tr.agreement td.up")).toExist();
-	});
+        it("each row should have an upvote cell", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements tr.agreement td.up")).toExist();
+        });
 
-	it("each row should have an upvote sentence cell", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements tr.agreement td.sentence")).toExist();
-	});
+        it("each row should have an upvote sentence cell", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements tr.agreement td.sentence")).toExist();
+        });
 
-	it("each row should have an votes cell", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements tr.agreement td.votes")).toExist();
-	});
+        it("each row should have an votes cell", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements tr.agreement td.votes")).toExist();
+        });
 
-	it("each row should have an downvote cell", function(){
-	    new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
-	    
-	    expect($("table.agreements tr.agreement td.down")).toExist();
-	});
+        it("each row should have an downvote cell", function(){
+            new Electus.AgreementsView({ model : agreements, el : $("#viewport") });
+            
+            expect($("table.agreements tr.agreement td.down")).toExist();
+        });
     });
 });
