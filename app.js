cff86c2de7f89c37d3c9e41be770187ad92dc870
dvberkel <daan.vanberkel@nedap.com>
2012-12-06 08:32:17 +0100

    go back to an older version of socket.io due to conflict with node
    see http://stackoverflow.com/questions/11889132/socket-io-client-library-gives-welcome-to-socket-io-message

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 var io = require("socket.io").listen(process.env.PORT || 3435);
 
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
-
 io.sockets.on('connection', function (socket) {
     socket.on('sentence', function(data){
 	console.log(data);
