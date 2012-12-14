cf6c6a6b5d5101892b26d029df669c6bce9b210f
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-12-14 10:43:48 +0100

    removed console.log. fixes issue #2

--- js/aggregator.js
+++ js/aggregator.js
@@ -2,7 +2,6 @@
     var socket = io.connect('http://techopinion.jit.su');
     var agreements = new Electus.Agreements();
     socket.on('agreement', function (data) {
-        console.log (data);
         agreements.addAgreements(data.sentence.text, data.agreement);
     });
 
